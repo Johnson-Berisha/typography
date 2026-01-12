faa2e559e11a20062ee3902b1e943e20eec992e8
Gjonson <gjonsonberisha@gmail.com>
2026-01-12 22:19:36 +0100

    got rid of test snippet

--- src/app/code-snippet-app/snippets-content/SnippetsProvider.jsx
+++ src/app/code-snippet-app/snippets-content/SnippetsProvider.jsx
@@ -11,7 +11,7 @@ export function SnippetProvider({ children }) {
     useEffect(() => {
         const fetchSnippets = async () => {
             try {
-                const fileNames = ["elegantheader", "test", "fake-snippet", "basic-config"];
+                const fileNames = ["elegantheader", "fake-snippet", "basic-config"];
                 const fetchedSnippets = await Promise.all(
                     fileNames.map(fileName =>
                         fetch(
