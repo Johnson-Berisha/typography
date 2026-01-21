0695288874f87bbdd8f15f6fb6c31f19e9fc6196
Gjonson <gjonsonberisha@gmail.com>
2026-01-21 19:05:23 +0100

    trying to make new header in docs

--- src/app/docs/page.js
+++ src/app/docs/page.js
@@ -72,6 +72,39 @@ export default function Home() {
 
   return (
     <div className="container docs-body">
+      <aside className="sidebar">
+        <div className="brand">
+          <div className="brand-title">
+            <Image
+              src="/logo-32-transparent.svg"
+              alt="Typography Logo"
+              width={32}
+              height={32}
+            />{" "}
+            <h1>Fontiq</h1>
+          </div>
+          <p className="brand-subtext">Code Snippets App</p>
+        </div>
+        <nav className="snippets-nav">
+          <p className="nav-label">Library</p>
+          {["All Snippets", "Header", "test", "Config"].map((type) => (
+            <button
+              key={type}
+            >
+              {type}
+            </button>
+          ))}
+        </nav>
+        <div className="sidebar-footer">
+          <Link href={"/"}>
+            <Image
+              src={"/home-alt.svg"}
+              alt="go home"
+              width={23}
+              height={23}
+            />Home</Link>
+        </div>
+      </aside>
       <div className="content">
 
         <div className="guides-demo">
